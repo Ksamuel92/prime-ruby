1f16db11f5d72ff4b36cc9453b0d65d8eb2d5987
Kyle Samuel <Ksamuel92@gmail.com>
2021-02-26 13:18:56 -0500

    commit tree 64f8a2d9c73cd48c7dc3a15559007c5d3fc921c2

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(int)
+   if (int.negative? == true || int < 2)
+    return false
+   end
+range_of_numbers = (2..int-1).to_a #> [2,3,4...int-1]
+range_of_numbers.each do |i|
+    return false if int % i == 0
+end
+true
+end
